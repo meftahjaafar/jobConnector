02f360d60443d1e6a967d85ca96bf85755e4062c
meftahjaafar <meftahjaafar@gmail.com>
2020-11-14 01:31:32 +0000

    create user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    avatar:{
+        type: String
+    },
+    date:{
+        type:Date,
+        default: Date.now
+    },
+    type:{
+        type:String
+    }
+    
+})
+
+module.exports = mongoose.moddel('user', UserSchema)
+
