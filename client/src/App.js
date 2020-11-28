7b3ce2878709b3e6e83faa105a67590173c58ff8
meftahjaafar <meftahjaafar@gmail.com>
2020-11-28 13:25:44 +0000

    create private route

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 
 
@@ -34,6 +36,7 @@ function App() {
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
