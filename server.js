7ef84f4958b4b6222fc8611c9d91e53343482f35
meftahjaafar <meftahjaafar@gmail.com>
2020-12-09 20:34:59 +0000

    prepare for deployment

--- server.js
+++ server.js
@@ -45,12 +45,13 @@ app.use('/api/activities', require('./routes/api/activity'))
 app.use('/api/sharedposts', require('./routes/api/sharedPost'))
 
 
-//@ Serve Static assets in production
-if (process.env.NODE_ENV === 'production'){
-//Set Static Folder
-app.use(express.static('client/build'))
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
-}
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
